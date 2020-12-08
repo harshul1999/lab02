fc834ad8d229e8488d6b8e0c165295c2eb689c06
harshul1999 <60629741+harshul1999@users.noreply.github.com>
2020-12-08 15:13:25 -0500

    Add files via upload

---
+++ src/components/About/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../shared/Header";
+
+const About = () => {
+  return (
+    <>
+      <Header title="About" />
+      {/* You need a paragraph describing a fun fact about you */}
+      {"Hi, Welcome to Harshul's programming world. I hate programming/deBugging. I can't be patient enough to debug."}
+    </>
+  );
+};
+
+export default About;
